8934464d7ed8be50682a7ea5be138bc0d6de0480
solobdev <sofi2darling@gmail.com>
2025-12-21 22:24:22 +0300

    Update HomePage layout and add flutter_svg dependency

--- fitness_spa/lib/pages/home.dart
+++ fitness_spa/lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,7 +7,32 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text(
+          'Breakfast',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          )
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        centerTitle: true,
+        leading: Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          child: SvgPicture.asset(
+            'assets/icons/Arrow - Left 2.svg',
+          ),
+          
+          decoration: BoxDecoration(
+            color: Color(0xFFF7F8F8),
+            borderRadius:  BorderRadius.circular(10),
+          ),
+        ),
+        actions: [],
+      ),
     );
   }
 }
