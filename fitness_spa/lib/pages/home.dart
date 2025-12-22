9c1a2ee940d5b463da8c70e376b5e1dd9ebb476f
solobdev <sofi2darling@gmail.com>
2025-12-22 10:22:15 +0300

    Enhance HomePage layout with search field and categories section

--- fitness_spa/lib/pages/home.dart
+++ fitness_spa/lib/pages/home.dart
@@ -7,7 +7,100 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      appBar: AppBar(
+      appBar: appBar(),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: .start,
+        children: [
+          _searchField(),
+          SizedBox(height: 40,),
+          Column(
+            crossAxisAlignment: .start,
+            children: [
+              // Categories Sectionr
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Text(
+                  'Categories',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: .bold,
+                  )
+                ),
+              ),
+              SizedBox(height: 15,),
+              Container(
+                height: 150,
+                color: Colors.green,
+              )
+            ],
+          )
+        ]
+      )
+    );
+  }
+
+  Container _searchField() {
+    return Container(
+          margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Color(0xFF1d1417).withOpacity(0.11),
+                blurRadius: 40,
+                spreadRadius: 0.0,
+
+              )
+            ]
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              contentPadding: EdgeInsets.all(15),
+              hintText: 'Search Pancakes',
+              hintStyle: TextStyle(
+                color: Color(0xFFDDDADA),
+                fontSize: 14,
+              ),
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(12),
+                child: SvgPicture.asset('assets/icons/Search.svg'),
+              ),
+              border: OutlineInputBorder(
+                borderRadius: .circular(15),
+                borderSide: .none,
+              ),
+              suffixIcon: Container(
+                width: 100,
+                child: IntrinsicHeight(
+                  child: Row(
+                    mainAxisAlignment: .end,
+                    children: [
+                      VerticalDivider(
+                        color: Colors.black,
+                        indent: 10,
+                        endIndent: 10,
+                        thickness: 0.1,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(12),
+                        child: SvgPicture.asset('assets/icons/Filter.svg'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ),
+        );
+  }
+}
+
+
+AppBar appBar() {
+        return AppBar(
         title: Text(
           'Breakfast',
           style: TextStyle(
@@ -19,20 +112,39 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.white,
         elevation: 0.0,
         centerTitle: true,
-        leading: Container(
-          margin: EdgeInsets.all(10),
-          alignment: Alignment.center,
-          child: SvgPicture.asset(
-            'assets/icons/Arrow - Left 2.svg',
-          ),
-          
-          decoration: BoxDecoration(
-            color: Color(0xFFF7F8F8),
-            borderRadius:  BorderRadius.circular(10),
+        leading: GestureDetector(
+          onTap: () {
+
+          },
+          child: Container(
+            margin: EdgeInsets.all(10),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Color(0xFFF7F8F8),
+              borderRadius:  BorderRadius.circular(10),
+            ),
+            child: SvgPicture.asset(
+              'assets/icons/Arrow - Left 2.svg',
+            ),
           ),
         ),
-        actions: [],
-      ),
-    );
-  }
+        actions: [
+          GestureDetector(
+            onTap: () {
+
+            },
+            child: Container(
+              margin: EdgeInsets.all(10),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Color(0xFFF7F8F8),
+                borderRadius:  BorderRadius.circular(10),
+              ),
+              child: SvgPicture.asset(
+                'assets/icons/dots.svg',
+              ),
+            ),
+          ),
+        ],
+      );
 }
